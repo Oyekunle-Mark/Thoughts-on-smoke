82913e4a48da14260a44ef71a9fdfd9b94fe90d0
oyekunle-mark <oyekunlemac@gmail.com>
2019-07-02 16:42:20 +0100

    Added breath first and depth first search to the graph data structure

--- graph/graph.js
+++ graph/graph.js
@@ -21,6 +21,79 @@ class Graph {
   print() {
     for (const [key, value] of this.AdjList) console.log(key, value);
   }
+
+  createdVisitedObject() {
+    const visited = {};
+
+    for (const i of this.AdjList.keys()) visited[i] = false;
+
+    return visited;
+  }
+
+  breathFirstSearch(startNode) {
+    const visited = this.createdVisitedObject();
+    const queue = [];
+
+    visited[startNode] = true;
+    queue.push(startNode);
+
+    while (queue.length) {
+      const currentVertex = queue.pop();
+      console.log(currentVertex);
+
+      const edges = this.AdjList.get(currentVertex);
+
+      for (const i of edges) {
+        if (!visited[i]) {
+          visited[i] = true;
+          queue.unshift(i);
+        }
+      }
+    }
+  }
+
+  depthFirstSearch(startingNode) {
+    const visited = this.createVisitedObject();
+    this.dfsHelper(startingNode, visited);
+  }
+
+  dfsHelper(startingNode, visited) {
+    visited[startingNode] = true;
+    console.log(startingNode);
+
+    const arr = this.AdjList.get(startingNode);
+
+    for (const elem of arr) {
+      if (!visited[elem]) {
+        this.dfsHelper(elem, visited);
+      }
+    }
+  }
+
+  doesPathExist(firstNode, secondNode) {
+    const path = [];
+    const visited = this.createVisitedObject();
+    const queue = [];
+    visited[firstNode] = true;
+    queue.push(firstNode);
+    while (queue.length) {
+      const node = queue.pop();
+      path.push(node);
+      const elements = this.AdjList.get(node);
+      if (elements.includes(secondNode)) {
+        console.log(path.join('->'));
+        return true;
+      }
+
+      for (const elem of elements) {
+        if (!visited[elem]) {
+          visited[elem] = true;
+          queue.unshift(elem);
+        }
+      }
+    }
+    return false;
+  }
 }
 
 const g = new Graph();
